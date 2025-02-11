5a30b232c4a38bd91dc740ecbf0f89f3941c41a4
Mike <obagangnongshita@gmail.com>
2025-02-11 10:54:42 +0800

    init

---
+++ HelloFramework.framework/Headers/HelloFramework.h
@@ -0,0 +1,24 @@
+//
+//  HelloFramework.h
+//  HelloFramework
+//
+//  Created by leagsoftEmmM201 on 2025/2/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloFramework.
+FOUNDATION_EXPORT double HelloFrameworkVersionNumber;
+
+//! Project version string for HelloFramework.
+FOUNDATION_EXPORT const unsigned char HelloFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloFramework/PublicHeader.h>
+
+
+@interface HelloFramework : NSObject
+
+- (void)helloWorld;
+
+@end
+
